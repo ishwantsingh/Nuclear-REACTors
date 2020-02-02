01bdc0c5568fd96b9856ec22c2f6aaa2c975695a
Ishwant Singh <ishwant8@gmail.com>
2020-02-02 14:12:30 +0530

    css: login page user svg.

--- trey-way/src/components/layout/Login.js
+++ trey-way/src/components/layout/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import user from "../../images/user.svg";
 
 const StyledLoginDiv = styled.div`
   width: 100%;
@@ -9,6 +10,21 @@ const StyledLoginDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  width: 23%;
+  height: 500px;
+  margin: 50px auto;
+  padding: 20px 10px;
+  background-color: #fafafa;
+  border-color: #e6e6e6;
+  border-radius: 8px;
+  border-style: solid;
+  border-width: 1px;
+`;
+
+const Image = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
 `;
 
 export function Login(props) {
@@ -16,6 +32,7 @@ export function Login(props) {
   if (!props.auth.uid) {
     return (
       <StyledLoginDiv>
+        <Image src={user} alt="metro" />
         <h3>Hmm... seems like you haven't logged in.</h3>
         <h4>Click the Login Button in the top-right to Login with Google!</h4>
       </StyledLoginDiv>
